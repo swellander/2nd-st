bc6ba6020cb871e9f1d72275096c9600d461fb7c
Sam Wellander <samwellander@gmail.com>
2018-07-11 12:33:55 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const db = [
+  {
+    title: 'Oceans 11'
+  },
+  {
+    title: 'My Big Fat Greek Wedding'
+  },
+  {
+    title: 'Deadpool'
+  }
+]
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.listen(3000, () => {console.log('Server started on port 3000')});
