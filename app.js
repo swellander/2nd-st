f04f4063d4d5bddca6f44a9ed4d97fc5f012ff13
Sam Wellander <samwellander@gmail.com>
2018-07-11 14:08:53 -0400

    baby steps

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const app = express();
-
-const db = [
-  {
-    title: 'Oceans 11'
-  },
-  {
-    title: 'My Big Fat Greek Wedding'
-  },
-  {
-    title: 'Deadpool'
-  }
-]
+const bodyParser = require('body-parser');
+const db = require('./db');
 
 app.set('view engine', 'ejs');
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {db});
+})
+
+app.post('/', urlencodedParser, (req, res) => {
+  db.push({title: req.body.title});
+  res.render('index', {db});
 })
 
 app.listen(3000, () => {console.log('Server started on port 3000')});
---
+++ db.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'Oceans 11'
+  },
+  {
+    title: 'My Big Fat Greek Wedding'
+  },
+  {
+    title: 'Deadpool'
+  },
+  {
+    title: 'The Great Escape'
+  }
+]
